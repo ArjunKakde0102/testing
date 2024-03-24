8e8d89f17076b921130e418b988a1d87071778b3
ArjunKakde0102 <157232528+ArjunKakde0102@users.noreply.github.com>
2024-03-24 12:02:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template ,  redirect, request, flash,url_for
+from flask import Flask,render_template ,  redirect, request
 app = Flask(__name__)
 @app.route('/')
 def index():
